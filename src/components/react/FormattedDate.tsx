a102a7b94c1796ca6278a46bf5bedcf02eb5a275
Jeff Musal <jmusal@gmail.com>
2025-02-03 18:59:43 -0500

    fix: Convert datetime attribute to dateTime in FormattedDate component (#3)

--- src/components/react/FormattedDate.tsx
+++ src/components/react/FormattedDate.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 function FormattedDate({ date }: { date: string }) {
 	const _date = new Date(date)
 	return (
-		<time datetime={_date.toISOString()}>
+		<time dateTime={_date.toISOString()}>
 			{
 				_date.toLocaleDateString('en-us', {
 					year: 'numeric',
