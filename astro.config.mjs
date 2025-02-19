e9a2ae6dc4b0d0dd8b57d628341097d587761a67
Jack Pettit [SSW] <57518417+JackDevAU@users.noreply.github.com>
2025-02-19 11:59:04 +1000

    Add env.example + site url env (#7)
    * Add env.example + site url env
    
    * add url
    
    * force-re-index
    
    * fix: add https, update url

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import tinaDirective from "./astro-tina-directive/register"
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
+	site: process.env.SITE_URL || `https://${process.env.VERCEL_URL}`,
 	integrations: [mdx(), sitemap(), react(), tinaDirective()],
 	redirects: {
 		'/admin': '/admin/index.html'
