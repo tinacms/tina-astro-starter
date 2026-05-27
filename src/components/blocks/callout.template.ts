625d9e67f88aa65dbea77bdb2b861956820fef5a
Josh Berman <joshberman@ssw.com.au>
2026-05-27 10:52:57 +1000

    feat: callout block

--- src/components/blocks/callout.template.ts
+++ src/components/blocks/callout.template.ts
@@ -1,6 +1,15 @@
 import type { Template } from 'tinacms';
+import { sectionBackgroundField } from '../../../tina/templates/section-background';
+
 export const calloutBlockSchema: Template = {
 	name: 'callout',
 	label: 'Callout',
-	fields: [{ type: 'string', name: 'title', label: 'Title' }],
+	fields: [
+		sectionBackgroundField as any,
+		{ type: 'string', label: 'Text', name: 'text' },
+		{ type: 'string', label: 'Url', name: 'url' },
+	],
+	ui: {
+		defaultItem: { url: 'https://tina.io/editorial-workflow', text: 'Support for live editing and editorial workflow' },
+	},
 };
